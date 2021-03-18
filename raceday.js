94f397175efdcf79107e7e79aad6317f6c990861
lairdsoft <wishywellweb@gmail.com>
2021-03-18 12:40:12 +0000

    rockpaperscissors

---
+++ raceday.js
@@ -0,0 +1,18 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+
+let earlyOrNot = true;
+
+let runnersAge = 19;
+    if (runnersAge >= 18 && earlyOrNot === true) {
+      console.log(raceNumber += 1000);
+    }
+    if (runnersAge >= 18 && earlyOrNot === true) {
+      console.log('Your race will start at 9.30am ' + raceNumber + '!')
+    } else if (runnersAge >= 18 && earlyOrNot === false) {
+      console.log('Your race will start at 11.30am ' + raceNumber + '!')
+    } else (runnersAge <= 18); {
+      console.log('Your race will start at 12.30pm ' + raceNumber + '!')
+    };
+
+    console.log(raceNumber(16))
